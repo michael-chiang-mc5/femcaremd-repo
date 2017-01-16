e8ab30a89e97f17e381cdbf223c1f11a105fe4e9
Michael Chiang <michaelchiang@Michaels-iMac.local>
2017-01-15 22:33:42 -0800

    Changed names of page templates to force update

--- js/content.js
+++ js/content.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
 
   // this is the landing page
-  $.get("locations.html", function( my_var ) {
+  $.get("locations_tab.html", function( my_var ) {
       $( "#content" ).html(my_var);
   });
 
@@ -11,44 +11,44 @@ $(document).ready(function() {
   $( "a" ).click(function(e) {
     var id = $(this).attr("id");
     if ( id == "home" ) {
-      $.get("home.html", function( my_var ) {
+      $.get("home_tab.html", function( my_var ) {
           $( "#content" ).html(my_var);
       });
     }
 
     else if ( id == "locations" ) {
-      $.get("locations.html", function( my_var ) {
+      $.get("locations_tab.html", function( my_var ) {
           $( "#content" ).html(my_var);
       });
     }
 
     else if ( id == "pricing" ) {
-      $.get("pricing.html", function( my_var ) {
+      $.get("pricing_tab.html", function( my_var ) {
           $( "#content" ).html(my_var);
       });
     }
 
     else if ( id == "about_the_doctors" ) {
-      $.get("doctor_bio.html", function( my_var ) {
+      $.get("doctor_bio_tab.html", function( my_var ) {
           $( "#content" ).html(my_var);
       });
     }
 
     else if ( id == "patient_info_sheets" ) {
-      $.get("home.html", function( my_var ) {
+      $.get("home_tab.html", function( my_var ) {
           $( "#content" ).html(my_var);
       });
     }
 
 
     else if ( id == "patient_feedback" ) {
-      $.get("patient_feedback.html", function( my_var ) {
+      $.get("patient_feedback_tab.html", function( my_var ) {
           $( "#content" ).html(my_var);
       });
     }
 
     else if ( id == "helpful_links" ) {
-      $.get("home.html", function( my_var ) {
+      $.get("home_tab.html", function( my_var ) {
           $( "#content" ).html(my_var);
       });
     }
